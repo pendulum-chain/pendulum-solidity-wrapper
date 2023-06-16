768fa63968caf44aeb1bb803004dd5fb8654a26c
Marcel Ebert <mail@marcel-ebert.de>
2023-06-16 16:55:06 +0200

    Implement transfer() function

--- erc20-wrapper/erc20-wrapper.sol
+++ erc20-wrapper/erc20-wrapper.sol
@@ -9,11 +9,16 @@ contract ERC20Wrapper {
     bytes1 private _variant;
     bytes1 private _index;
 
-    constructor(string memory name_, string memory symbol_, uint8 decimals_, bytes1 variant_, bytes1 index_) {
+    // `0` indicates OriginType 'caller'
+    // everything else indicates OriginType 'address'
+    uint8 private _originType;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, uint8 originType_, bytes1 variant_, bytes1 index_) {
         _name = name_;
         _symbol = symbol_;
         _decimals = decimals_;
 
+        _originType = originType_;
         _variant = variant_;
         _index = index_;
     }
@@ -54,7 +59,7 @@ contract ERC20Wrapper {
         return totalSupplyU256;
     }
 
-    function balanceOf(address _owner) public returns (uint256 balance) {
+    function balanceOf(address _owner) public returns (uint256) {
         // Encode currency and address
         bytes currency = createCurrencyId();
         bytes owner = abi.encode(_owner);
@@ -74,8 +79,25 @@ contract ERC20Wrapper {
         return balanceU256;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        return false;
+    function transfer(address _to, uint256 _amount) public returns (bool) {
+//        bytes from = abi.encode(msg.sender);
+        bytes origin = abi.encode(_originType);
+        bytes currency = createCurrencyId();
+        bytes to = abi.encode(_to);
+        bytes amount = abi.encode(_amount);
+
+        bytes input = abi.encodePacked(origin, currency, to, amount);
+        print("input: {}".format(input));
+        (uint32 result_chain_ext, bytes raw_data) = chain_extension(1105, input);
+        print("result_chain_ext: {}".format(result_chain_ext));
+        print("raw_data: {}".format(raw_data));
+
+        bool success = result_chain_ext == 0;
+//        require(result_chain_ext == 0, "Call to chain_extension failed.");
+
+//        bool success = abi.decode(raw_data, (bool));
+        print("success: {}".format(success));
+        return success;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
@@ -90,7 +112,7 @@ contract ERC20Wrapper {
         return 0;
     }
 
-    function createCurrencyId() public returns (bytes) {
+    function createCurrencyId() public view returns (bytes) {
         bytes memory currency = new bytes(0);
         // We use the knowledge we have about our `CurrencyId` enum to craft the encoding
         if (_variant == 0) {
