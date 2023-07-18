b54848f713f5e8f6b64b585dee2d9be8715f47ed
Marcel Ebert <mail@marcel-ebert.de>
2023-07-18 12:03:51 +0200

    Remove `pragma` definitions (#28)
    * Remove `pragma` definitions
    because they are not actually used in Solang, see
    https://solang.readthedocs.io/en/latest/language/pragmas.html
    
    * Add comments explaining missing pragma definition

--- price-oracle-wrapper/interfaces/IPriceOracleGetter.sol
+++ price-oracle-wrapper/interfaces/IPriceOracleGetter.sol
@@ -1,6 +1,8 @@
 // Based on AAVE protocol
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
+
+// This file does not define a pragma version because it is meant to be compiled with Solang and Solang ignores
+// pragma definitions, see [here](https://solang.readthedocs.io/en/latest/language/pragmas.html).
 
 /// @title IPriceOracleGetter interface
 interface IPriceOracleGetter {
